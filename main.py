b64eff324db907375193c1c008aad47ba7619624
robel <robelwon75@gmail.com>
2024-02-13 16:53:11 +0300

    rename main file

--- main.py
+++ main.py
@@ -358,4 +358,4 @@ def addassignment(class_id):
 # ================== End Class room ===============================
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
